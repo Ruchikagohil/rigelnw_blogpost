70a3eadf6b876880f4cb052b547f5ad7d600e7c4
Ruchika Gohil <ruchikagohel32@gmail.com>
2019-11-20 21:09:12 +0530

    alter user tbl & seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123456'),
+            'user_type' => 'admin'
+        ]);
+    }
+}
